b8e1fa0a3eb0192b3e7f753f4874d83debfd9670
Carrington <Manyuchi>
2023-07-08 21:10:03 +0200

    adding textFields in UIAlert Controller

--- AlertController/ViewController.swift
+++ AlertController/ViewController.swift
@@ -22,12 +22,29 @@ class ViewController: UIViewController {
         let alertController = UIAlertController(title: "Hello Alert", message: "It is my first Alert", preferredStyle: .alert)
         
         alertController.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+            
+            
+            // retrieving text of Title of action
             print(action.title)
+            
+            // retrieving text of first textField
+            let textFieldText = alertController.textFields?[0].text
+            let textFieldText_2 = alertController.textFields?[1].text
+            print(textFieldText! + " " + textFieldText_2!)
+            
         })
         
         alertController.addAction(UIAlertAction(title: "Delete", style: .destructive){ _ in
              print("delete button tapped")
         })
+        
+        var textFieldText = ""
+
+        // Adding UITextField to AlertController
+        alertController.addTextField { textfield in
+            textfield.placeholder = "Login"
+        }
+        alertController.addTextField()
         
         alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
