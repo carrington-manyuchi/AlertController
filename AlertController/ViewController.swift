51ff8ef5d251b312dd713670d90ed91b803d3b64
Carrington <Manyuchi>
2023-07-08 20:33:49 +0200

    created simple alert

--- AlertController/ViewController.swift
+++ AlertController/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var click: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func btnTapped(_ sender: UIButton) {
+        
+        //declaring alert controller
+        let alertController = UIAlertController(title: "Hello Alert", message: "It is my first Alert", preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default) { action in
+            print(action.title)
+        })
+        
+        alertController.addAction(UIAlertAction(title: "Delete", style: .destructive){ _ in
+             print("delete button tapped")
+        })
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        
+        // presenting alert controller
+        self.present(alertController, animated: true) {
+            self.view.backgroundColor = .red
+        }
+        
+    }
+    
 }
 
